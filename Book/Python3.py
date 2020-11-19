e4ac845d0b192d9cbba8e4d33436e21ce77a962a
Roman Zamkovoy <romanzamkovoy2010@gmail.com>
2020-11-19 21:34:52 +0300

    Created Python 3

---
+++ Book/Python3.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+import random
+import time
+
+odds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19,
+        20, 21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39,
+        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 58, 59, 60]
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute.")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
