dedaf31dc587831c4ba382571f9eb8f71bd43f54
Roman Zamkovoy <romanzamkovoy2010@gmail.com>
2021-04-17 18:22:00 +0300

    Cross Zero V2

--- Lessons/cross_zero_V2.py
+++ Lessons/cross_zero_V2.py
@@ -2,13 +2,23 @@ a = [ ["_","_","_"], ["_","_","_"],  ["_","_","_"]]
 robot = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 import random
 
+def lose(a):
+    for i in range(3):
+        if (a[i][0] == "x" and a[i][1] == "x" and a[i][2] == "_"):  # СТРОКа
+            return 1
+        if (a[i][0] == "x" and a[i][2] == "_" and a[i][2] == "x"):  # СТРОКа
+            return 1
+        if (a[i][1] == "_" and a[i][2] == "x" and a[i][2] == "x"):  # СТРОКа
+            return 1
+    # ДЗ - сделать такую же проверку по столбцам и _ДИАГОНАЛИ_
+
 def win (a):
     if (a[0][0] == a[0][1] and a[0][1] == a[0][2]):  # СТРОКИ
         if (a[0][0] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[0][2] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
 
     if (a[1][0] == a[1][1] and a[1][1] == a[1][2]):
@@ -16,49 +26,49 @@ def win (a):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[1][2] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     if (a[2][0] == a[2][1] and a[2][1] == a[2][2]):
         if (a[2][0] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[2][2] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     if (a[0][0] == a[1][1] and a[1][1] == a[2][2]):  # ДИАГОНАЛИ
         if (a[0][0] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[2][2] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     if (a[0][2] == a[1][1] and a[1][1] == a[2][0]):
         if (a[0][2] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[0][2] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     if (a[0][0] == a[1][0] and a[1][0] == a[2][0]):  # СТОЛБЦЫ
         if (a[0][0] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[1][0] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     if (a[0][1] == a[1][1] and a[1][1] == a[2][1]):
         if (a[0][1] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif (a[1][1] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     if (a[0][2] == a[1][2] and a[1][2] == a[2][2]):
         if (a[0][2] == 'x'):
             print("Игрок 1 ПОБЕДИЛ!")
             return 1
         elif(a[0][2] == 'O'):
-            print("Игрок 2 ПОБЕДИЛ!")
+            print("БАРСИК ПОБЕДИЛ!")
             return 1
     return 0
 
@@ -88,8 +98,10 @@ for k in range(4):
 
     if(win(a) == 1):
         exit()
-    print("Ходит робот...")
+    print("Ходит БАРСИК...")
     hod = random.choice(robot)
+    if(lose(a) == 1):
+        hod =
     hod = hod - 1
     print(hod + 1)
     a[hod//3][hod%3] = "O"
@@ -98,6 +110,9 @@ for i in range(len(a)):
     for x in range(len(a[i])):
         print(a[i][x],end = " ")
     print()
+if(win(a) == 1):
+    exit()
+
 print("Игрок 1 ходит")
 n = int(input())
 while n > 9 or n < 1:
@@ -113,7 +128,6 @@ for i in range(len(a)):
     for x in range(len(a[i])):
         print(a[i][x], end=" ")
     print()
-
 if (win(a) == 1):
     exit()
 
