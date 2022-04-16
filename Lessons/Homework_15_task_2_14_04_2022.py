009a539b6896fe792b3d49893098b33c752c3cf0
Max Zamkovoy <max.zamkovoy@gmail.com>
2022-04-16 13:21:52 +0300

    Homework 15 task

--- Lessons/Homework_15_task_2_14_04_2022.py
+++ Lessons/Homework_15_task_2_14_04_2022.py
@@ -1,11 +1,11 @@
 a = int(input())
-if a == 1 or a == 2 or a == 12:
+if a in [1, 2, 12]:
     print('Зима')
-if a == 3 or a == 4 or a == 5:
+if a in [3, 4, 5]:
     print('Весна')
-if a == 6 or a == 7 or a == 8:
+if a in [6, 7, 8]:
     print('Лето')
-if a == 9 or a == 10 or a == 11:
+if a in [9, 10, 11]:
     print('Осень')
-if a > 12:
+if a > 12 and a < 1:
     print('Месецев всего 12, напишите число от 1 до 12')
