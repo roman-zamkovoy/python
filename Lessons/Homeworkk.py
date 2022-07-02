baa7da9be9389f6e7e6d34aaacfcb9b59945cf74
Max Zamkovoy <max.zamkovoy@gmail.com>
2022-07-02 20:36:10 +0300

    Homework 16 24_03_2022_12:30

--- Lessons/Homeworkk.py
+++ Lessons/Homeworkk.py
@@ -1,1 +1,13 @@
-print(len(max(input().split('1'), key = len)))
+#a = int(input())
+#for i in range(1, a + 1)
+#    if a % i == 0:
+#        print(i)
+p = 8
+for i in range(2000, 3660):
+    j = []
+    for h in range(1, i + 1):
+        if i % h == 0:
+            j.append(h)
+    if p not in j:
+        print(i)
+
