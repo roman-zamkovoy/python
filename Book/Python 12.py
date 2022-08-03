1245b37916d87712515f2060c49d5797d72bf0c8
Roman Zamkovoy <romanzamkovoy2010@gmail.com>
2022-08-03 19:53:59 +0300

    Матвей проверь пожалуйста задания из книжки

---
+++ Book/Python 12.py
@@ -0,0 +1,10 @@
+def double(arq):
+    print('Before: ', arq)
+    arq = arq * 2
+    print('After:  ', arq)
+
+
+def change(arq):
+    print('Before: ', arq)
+    arq.append('More data')
+    print('After:  ', arq)
